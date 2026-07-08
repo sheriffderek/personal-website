029d2f484f9440978173b79e9d354e8d69e1505d
sheriffderek <derek@sheriffderek.consulting>
2026-07-07 17:26:11 -0700

    Save progress

--- includes/settings/filter-control.php
+++ includes/settings/filter-control.php
@@ -5,7 +5,5 @@
 		<input type='range' min='1' max='3' step='1' value='1' data-set-filter class='plain-range' aria-label='Filter level'>
 
 		<ol class='mini-map' aria-hidden='true'></ol>
-
-		<div class='mini-rail' aria-hidden='true'></div>
 	</div>
 </div>
