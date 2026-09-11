9322da49644a52a03f547ee64992a81276c488cb
sheriffderek <derek@sheriffderek.consulting>
2026-09-11 15:09:46 -0700

    The past-writing shelf grows into the real thing
    Derek's full link set, all verified live without following redirects:
    Answering questions (Stack Overflow - 14 years, 2.3M people reached -
    Quora's ~1,500 answers, dev.to), Articles (CSS-Tricks, the Substack),
    Perpetual Education (the hundreds-of-workshops blanket line plus five
    samplers chosen to show range: a core UI principle, the designer-
    developer analogy, Figma variables tooling, a timeboxed learning
    sprint, a story), Every week (open office hours, the CSS Discord),
    and On camera (the Don the Developer episode). Group headings carry
    the section rhythm; Derek's card treatment still comes later.

--- templates/pages/journal.php
+++ templates/pages/journal.php
@@ -45,7 +45,39 @@
 
 		<h2 class='attention-voice'>Earlier writing, elsewhere</h2>
 
-		<p>This journal is new; the writing isn't. Before consolidating here, it lived in other places:</p>
+		<p>This journal is new; the writing isn't. Before consolidating here, it lived all over:</p>
+
+		<h3 class='strong-voice'>Answering questions</h3>
+
+		<ul>
+
+			<li>
+
+				<a class='link' href='https://stackoverflow.com/users/1399456/sheriffderek' target='_blank'>Stack Overflow</a>
+
+				<p class='quiet-voice'>14 years, 2.3 million people reached - mostly HTML, CSS, and how the web actually works.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://www.quora.com/profile/Derek-Thomas-Wood' target='_blank'>Quora</a>
+
+				<p class='quiet-voice'>Around 1,500 answers about design, code, and learning.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://dev.to/sheriffderek' target='_blank'>dev.to</a>
+
+				<p class='quiet-voice'>Posts and conversations in the dev community.</p>
+
+			</li>
+
+		</ul>
+
+		<h3 class='strong-voice'>Articles</h3>
 
 		<ul>
 
@@ -65,11 +97,87 @@
 
 			</li>
 
+		</ul>
+
+		<h3 class='strong-voice'>Perpetual Education</h3>
+
+		<p>Hundreds of workshops for the school, and hundreds more resources, stories, and exercises around them. A few that show the range:</p>
+
+		<ul>
+
 			<li>
 
-				<a class='link' href='https://perpetual.education/blog/' target='_blank'>The Perpetual Education blog</a>
+				<a class='link' href='https://perpetual.education/resources/almost-all-user-interface-comes-down-to-this/' target='_blank'>Almost all user interface comes down to this</a>
 
-				<p class='quiet-voice'>Years of writing from running the school - lessons, stories, and teaching material.</p>
+				<p class='quiet-voice'>A core UI principle, taught in one sitting.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/an-analogy-for-the-designer-developer-relationship/' target='_blank'>An analogy for the designer-developer relationship</a>
+
+				<p class='quiet-voice'>The seam this whole career keeps working on.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/resources/figma-variable-collection-composition/' target='_blank'>Figma variable collection composition</a>
+
+				<p class='quiet-voice'>Tooling deep-dives - design tokens where designers actually work.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/resources/visual-design-foundations-fast/' target='_blank'>Visual design foundations, fast</a>
+
+				<p class='quiet-voice'>A timeboxed learning sprint - the teaching-format experiments.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/stories/snow-fall/' target='_blank'>Snow Fall</a>
+
+				<p class='quiet-voice'>A story from the school.</p>
+
+			</li>
+
+		</ul>
+
+		<h3 class='strong-voice'>Every week</h3>
+
+		<ul>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/open-office-hours/' target='_blank'>Open office hours</a>
+
+				<p class='quiet-voice'>Weekly public sessions - bring a design or code question, leave with a plan.</p>
+
+			</li>
+
+			<li>
+
+				<a class='link' href='https://discord.gg/css' target='_blank'>The CSS Discord</a>
+
+				<p class='quiet-voice'>Helping out in the community where the CSS questions live now.</p>
+
+			</li>
+
+		</ul>
+
+		<h3 class='strong-voice'>On camera</h3>
+
+		<ul>
+
+			<li>
+
+				<a class='link' href='https://perpetual.education/stories/is-your-portfolio-doing-its-job-with-don-the-developer/' target='_blank'>Is your portfolio doing its job?</a>
+
+				<p class='quiet-voice'>With Don the Developer - the first of the podcast conversations.</p>
 
 			</li>
 
